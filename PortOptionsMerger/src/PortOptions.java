95cafc060a7704f2bee62857699c57ef7d1b4e2a
Dmytro Bilokha <dmytro@posteo.net>
2017-12-29 20:02:03 +0200

    Refactored, implemented global options from the make.conf parsing

--- PortOptionsMerger/src/PortOptions.java
+++ PortOptionsMerger/src/PortOptions.java
@@ -1,32 +1,17 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PortOptions {
 
-    private static final Pattern OPTION_PATTERN = Pattern.compile("[\\p{Upper}\\p{Digit}]+");
+    private static final Pattern OPTION_PATTERN = Pattern.compile("[\\p{Upper}\\p{Digit}_]+");
     private static final String NEW_LINE = System.lineSeparator();
     private static final String NEW_OPTION_LINE = "\\" + System.lineSeparator() + "\t\t";
     private static final int NEW_LINE_TRESHOLD = 80 - 15;
 
-    private final Set<String> setOptions = new HashSet<>();
-    private final Set<String> unsetOptions = new HashSet<>();
-
-    public PortOptions setOption(String option) {
-        unsetOptions.remove(option);
-        setOptions.add(option);
-        return this;
-    }
-
-    public PortOptions unsetOption(String option) {
-        setOptions.remove(option);
-        unsetOptions.add(option);
-        return this;
-    }
+    private final Map<String, Status> optionsMap = new HashMap<>();
 
     public PortOptions addOptions(Status status, String[] options) {
         if (status != Status.SET && status != Status.UNSET)
@@ -34,44 +19,52 @@ public class PortOptions {
         for (String option : options) {
             if (!OPTION_PATTERN.matcher(option).matches())
                 continue;
-            if (status == Status.SET)
-                setOption(option);
-            if (status == Status.UNSET)
-                unsetOption(option);
+            optionsMap.put(option, status);
         }
         return this;
     }
 
     public String getSetOptionsString() {
-        return formatOptions(setOptions);
+        return formatOptions(getSortedOptionsWithStatus(Status.SET));
+    }
+
+    private List<String> getSortedOptionsWithStatus(Status status) {
+        return optionsMap.entrySet()
+                .stream()
+                .filter(es -> es.getValue() == status)
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     public String getUnsetOptionsString() {
-        return formatOptions(unsetOptions);
+        return formatOptions(getSortedOptionsWithStatus(Status.UNSET));
     }
 
-    private String formatOptions(Collection<String> optionsToFormat) {
-        List<String> options = new ArrayList<>(optionsToFormat);
-        Collections.sort(options);
+    private String formatOptions(List<String> options) {
         StringBuilder outputBuilder = new StringBuilder("\t");
+        int lineLength = 0;
         for (String option : options) {
-            if (outputBuilder.length() + option.length() > NEW_LINE_TRESHOLD && option.length() < NEW_LINE_TRESHOLD) {
+            if (lineLength + option.length() > NEW_LINE_TRESHOLD && option.length() < NEW_LINE_TRESHOLD) {
                 outputBuilder.append(NEW_OPTION_LINE);
+                lineLength = 0;
             }
             outputBuilder
                     .append(option)
                     .append(' ');
+            lineLength += option.length() + 1;
         }
         outputBuilder.append(NEW_LINE);
         return outputBuilder.toString();
     }
 
     public Status getOptionStatus(String option) {
-        if (setOptions.contains(option))
-            return Status.SET;
-        if (unsetOptions.contains(option))
-            return Status.UNSET;
-        return Status.NOT_DEFINED;
+        return optionsMap.getOrDefault(option, Status.NOT_DEFINED);
+    }
+
+    public String getFormatted(String setPrefix, String unsetPrefix) {
+        return setPrefix + getSetOptionsString()
+                + unsetPrefix + getUnsetOptionsString();
     }
 
     public enum Status {
--- PortOptionsMerger/src/PortOptionsMerger.java
+++ PortOptionsMerger/src/PortOptionsMerger.java
@@ -1,11 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class PortOptionsMerger {
 
     private static final String BASE_PATH = "/usr/local/etc/poudriere.d/";
     private static final String MAKEFILE_SUFFIX = "-make.conf";
     private static final String OPTIONS_DIRECTORY_SUFFIX = "-options";
     private static final String CATEGORY_NAME_SPLITTER = "_";
+    private static final String MAKEFILE_SET_PREFIX = "OPTIONS_SET+=";
+    private static final String MAKEFILE_UNSET_PREFIX = "OPTIONS_UNSET+=";
+
+    private final String makeFileLocation;
+    private final String optionsDirectoryLocation;
+
+    public PortOptionsMerger(String prefix) {
+        this.makeFileLocation = BASE_PATH + prefix + MAKEFILE_SUFFIX;
+        this.optionsDirectoryLocation = BASE_PATH + prefix + OPTIONS_DIRECTORY_SUFFIX;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        if (args.length != 1) {
+            System.err.println("Provide one option - file/directory prefix");
+            return;
+        }
+        PortOptionsMerger merger = new PortOptionsMerger(args[0]);
+        merger.merge();
+    }
+
+    public void merge() {
+        OptionsParser globalOptionsParser = new OptionsParser(MAKEFILE_SET_PREFIX, MAKEFILE_UNSET_PREFIX);
+        PortOptions globalOptions = parseOptionsFile(makeFileLocation, globalOptionsParser);
+        System.out.print(globalOptions.getFormatted(MAKEFILE_SET_PREFIX, MAKEFILE_UNSET_PREFIX));
+    }
+
+    private PortOptions parseOptionsFile(String fileLocation, OptionsParser optionsParser) {
+        try {
+            Stream<String> fileLinesStream = Files.lines(Paths.get(fileLocation));
+            return optionsParser.parse(fileLinesStream.iterator());
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to read file " + fileLocation, e);
+        }
     }
 }
